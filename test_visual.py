a441e45b95932db5eb87640029839df010b58196
spx <2039219283@qq.com>
2024-04-15 21:13:05 +0800

    update3

--- test_visual.py
+++ test_visual.py
@@ -12,12 +12,12 @@ img = cv.imread('D:/test coding/000001.png')
 transf = transforms.ToTensor()
 img_tensor = transf(img)  # tensor数据格式是torch(C,H,W)
 
-
-
+T_poi = 20
 poi_en = PoissonEncoder()
 poi_out = poi_en(img_tensor)
-# print("PoissonEncoder:", poi_out)
-
+for t in range(T_poi):
+    # 在每个时间步调用poi_en(x)
+    print("PoissonEncoder:", poi_out)
 
 
 T_lat = 800
@@ -84,57 +84,75 @@ def plot_spike_trains(
 
 
 # #####################################################################泊松编码#####################################################################
-poi_out_flat = reshaped = poi_out.view(poi_out.shape[0], poi_out.shape[1]*poi_out.shape[2], 1)
-poi_out_transformed = poi_out_flat.repeat(1, 1, 10)
 
-for n_ex in range(poi_out_transformed .shape[0]):
-    Poi_visual = plot_spike_trains(poi_out_transformed, n_ex)
+T_poi = 20
+poi_en = PoissonEncoder()
+
+poi_list = []
+for t in range(T_poi):
+    # 在每个时间步调用poi_en(x)并更新poi_list
+    poi_out = poi_en(img_tensor).unsqueeze(0)
+    poi_list.append(poi_out)
+
+# 合并所有时间步的脉冲输出
+poi_list = torch.cat(poi_list, 0)
+#
+poi_out_flat = poi_list.view(poi_list.shape[0], poi_list.shape[1], -1)
+
+d_anti_seq = list(range(1, poi_out_flat.ndim))
+d_anti_seq.insert(2, poi_out_flat.ndim-3)
+poi_out_ter = poi_out_flat.permute(d_anti_seq)
+
+
+for n_ex in range(poi_out_ter.shape[0]):
+    poi_visual = plot_spike_trains(poi_out_ter, n_ex)
 
 # #####################################################################延迟编码#####################################################################
-
-T_lat = 800
-lat_en = LatencyEncoder(T_lat)
-
-lat_list = []
-for t in range(T_lat):
-    # 在每个时间步调用lat_en(x)并更新lat_list
-    lat_out = lat_en(img_tensor).unsqueeze(0)
-    lat_list.append(lat_out)
+#
+# T_lat = 800
+# lat_en = LatencyEncoder(T_lat)
+#
+# lat_list = []
+# for t in range(T_lat):
+#     # 在每个时间步调用lat_en(x)并更新lat_list
+#     lat_out = lat_en(img_tensor).unsqueeze(0)
+#     lat_list.append(lat_out)
 
 # 合并所有时间步的脉冲输出
-lat_list = torch.cat(lat_list, 0)
-lat_out_flat = reshaped = lat_list.view(lat_list.shape[0], lat_list.shape[1], -1)
-
-d_anti_seq = list(range(1, lat_out_flat.ndim))
-d_anti_seq.insert(2, lat_out_flat.ndim-3)
-lat_one_hot = lat_out_flat.permute(d_anti_seq)
-
-
-for n_ex in range(lat_one_hot.shape[0]):
-    lat_visual = plot_spike_trains(lat_one_hot, n_ex)
+# lat_list = torch.cat(lat_list, 0)
+# lat_out_flat = lat_list.view(lat_list.shape[0], lat_list.shape[1], -1)
+#
+# d_anti_seq = list(range(1, lat_out_flat.ndim))
+# d_anti_seq.insert(2, lat_out_flat.ndim-3)
+# lat_one_hot = lat_out_flat.permute(d_anti_seq)
+#
+#
+# for n_ex in range(lat_one_hot.shape[0]):
+#     lat_visual = plot_spike_trains(lat_one_hot, n_ex)
 
 # #####################################################################次序编码#####################################################################
-
-T_rank = 784
-rank_en = Rank_order_Encoder(T_rank)
-
-rank_list = []
-for t in range(T_rank):
-    # 在每个时间步调用rank_en(x)并更新rank_list
-    rank_out = rank_en(img_tensor).unsqueeze(0)
-    rank_list.append(rank_out)
-
-
-# 合并所有时间步的脉冲输出
-rank_list = torch.cat(rank_list, 0)
-
-rank_out_flat = reshaped = rank_list.view(rank_list.shape[0], rank_list.shape[1], -1)
-
-d_anti_seq = list(range(1, rank_out_flat.ndim))
-d_anti_seq.insert(2, rank_out_flat.ndim-3)
-rank_one_hot = rank_out_flat.permute(d_anti_seq)
-
-
-for n_ex in range(rank_one_hot.shape[0]):
-    rank_visual = plot_spike_trains(rank_one_hot, n_ex)
-
+#
+# T_rank = 784
+# rank_en = Rank_order_Encoder(T_rank)
+#
+# rank_list = []
+# for t in range(T_rank):
+#     # 在每个时间步调用rank_en(x)并更新rank_list
+#     rank_out = rank_en(img_tensor).unsqueeze(0)
+#     rank_list.append(rank_out)
+#
+#
+# # 合并所有时间步的脉冲输出
+# rank_list = torch.cat(rank_list, 0)
+# print("rank_list:", rank_list)
+# print("rank_list.size:", rank_list.size())
+#
+# rank_out_flat = rank_list.view(rank_list.shape[0], rank_list.shape[1], -1)
+#
+# d_anti_seq = list(range(1, rank_out_flat.ndim))
+# d_anti_seq.insert(2, rank_out_flat.ndim-3)
+# rank_one_hot = rank_out_flat.permute(d_anti_seq)
+#
+#
+# for n_ex in range(rank_one_hot.shape[0]):
+#     rank_visual = plot_spike_trains(rank_one_hot, n_ex)
