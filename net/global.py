7c58b0585115ff1942b5bb5ae6e04bc96e351523
Yang yajie <163796261+yangyajie0615@users.noreply.github.com>
2024-09-24 21:49:07 +0800

    Update global.py

--- net/global.py
+++ net/global.py
@@ -8,23 +8,9 @@ import torch.nn as nn
 from spikingjelly.Dataset_encoding import MNIST_encoder, Cifar10_encoder, Cifar100_encoder, FashionMNIST_encoder
 
 
-def train_global(device, batch_size, time, dataset_name:str = 'MNIST', encoder:str = 'poisson'):
+def train_global(net,device, batch_size, time, dataset_name:str = 'MNIST', encoder:str = 'poisson'):
     num = 0
-    net = nn.Sequential(
-    layer.Conv2d(1,10,5,bias=False),
-    STBP.LIFSpike(),
-    layer.MaxPool2d(2),
-    layer.Conv2d(10,20,5,bias=False),
-    STBP.LIFSpike(),
-    layer.MaxPool2d(2),
-    layer.Conv2d(20,40,5,padding = 2,bias=False),
-    STBP.LIFSpike(),
-    layer.MaxPool2d(2),
-    layer.Flatten(),
-    layer.Linear(40*2*2, 10, bias=False),
-    STBP.LIFSpike(),
-
-    ).to(device)
+    net = net
     functional.set_step_mode(net, 'm')
     loader = get_loader('train', dataset_name, encoder, batch_size, time)
     optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
@@ -46,24 +32,10 @@ def train_global(device, batch_size, time, dataset_name:str = 'MNIST', encoder:s
         functional.reset_net(net)
     torch.save(net.state_dict(), "net_train" + ".net")
 
-def test_global(device, batch_size, time, dataset_name:str = 'MNIST', encoder:str = 'poisson'):
+def test_global(net,device, batch_size, time, dataset_name:str = 'MNIST', encoder:str = 'poisson'):
     with torch.no_grad():
         train_acc = 0
-        net =nn.Sequential(
-        layer.Conv2d(1,10,5,bias=False),
-        STBP.LIFSpike(),
-        layer.MaxPool2d(2),
-        layer.Conv2d(10,20,5,bias=False),
-        STBP.LIFSpike(),
-        layer.MaxPool2d(2),
-        layer.Conv2d(20,40,5,padding = 2,bias=False),
-        STBP.LIFSpike(),
-        layer.MaxPool2d(2),
-        layer.Flatten(),
-        layer.Linear(40*2*2, 10, bias=False),
-        STBP.LIFSpike(),
-
-        ).to(device)
+        net = net
         net.load_state_dict(torch.load("net_train" + ".net"))
         functional.set_step_mode(net, 'm')
         loader = get_loader('test',dataset_name, encoder, batch_size, time)
@@ -135,6 +107,3 @@ def get_loader(phase, dataset_name, encoder, batch_size, time):
 
 
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-train_global(device=device, batch_size=60, time=30,)
-test_global(device=device, batch_size=60, time=30,)
